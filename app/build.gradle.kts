84bc895fc7821b119cdfab0f346e1a62ee42ee20
NicoVitoriano001 <nicovitoriano@gmail.com>
2023-11-18 14:14:49 -0300

    AppListaCurso - MVC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.applistacurso"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.applistacurso"
